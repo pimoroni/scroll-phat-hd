81813de25a35c61b7aa258f909d75973d8e5cd60
Phil Howard <phil@gadgetoid.com>
2017-01-27 16:00:10 +0000

    Scrolling test

--- scrolltest.py
+++ scrolltest.py
@@ -1,5 +1,4 @@
 import time
-import math
 
 import smbus
 import is31fl3731
@@ -7,11 +6,14 @@ import is31fl3731
 i2c = smbus.SMBus(1)
 display = is31fl3731.ScrollPhatHD(i2c)
 
-speed_factor = 4
+
+display.pixel(0,0,64)
 
 try:
     while True:
-        scale = (math.sin(time.time() * speed_factor) + 1) / 2
-        display.fill(int(64 * scale))
+        display.scroll(-1,-1)
+        display.show()
+        time.sleep(0.1)
 except KeyboardInterrupt:
     display.fill(0)
+    display.show()
