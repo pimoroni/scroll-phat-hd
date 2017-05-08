af079653100ab7066c32ea2e3d02fb929040b32a
Phil Howard <phil@gadgetoid.com>
2017-05-08 14:48:50 +0100

    Updated and commented portraitclock.py

--- examples/portraitclock.py
+++ examples/portraitclock.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import time
-import signal
 
 import scrollphathd
 from scrollphathd.fonts import font5x5
@@ -21,15 +20,29 @@ scrollphathd.rotate(270)
 while True:
     scrollphathd.clear()
 
-    str_time = time.strftime("%H") 
-    scrollphathd.write_string(str_time, x=0, y=0, font=font5x5)
+    # See https://docs.python.org/2/library/time.html
+    # for more information on what the time formats below do.
 
-    str_time = time.strftime("%M")
-    scrollphathd.write_string(str_time, x=0, y=6, font=font5x5)
+    # Display the hour as two digits
+    scrollphathd.write_string(
+        time.strftime("%H"),
+        x=0,
+        y=0,
+        font=font5x5)
 
-    str_time = time.strftime("%S")
-    scrollphathd.write_string(str_time, x=0, y=12, font=font5x5)
+    # Display the minute as two digits
+    scrollphathd.write_string(
+        time.strftime("%M"),
+        x=0,
+        y=6,
+        font=font5x5)
+
+    # Display the second as two digits
+    scrollphathd.write_string(
+        time.strftime("%S"),
+        x=0,
+        y=12,
+        font=font5x5)
 
     scrollphathd.show()
     time.sleep(0.1)
-
