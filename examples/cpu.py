e073a9af1d611130c0071252bc35920805232e0d
Wesley Archer <raspberrycoulis@users.noreply.github.com>
2017-09-24 19:28:45 +0100

    Update cpu.py
    Added options to turn off display when script is stopped, and added additional comments to the code for learning purposes.

--- examples/cpu.py
+++ examples/cpu.py
@@ -2,38 +2,46 @@
 
 import sys
 import time
+import signal   # Used to turn off Scroll pHAT HD if auto started using systemd
 
+# Required to define the clear on shutdown if auto started using systemd
+def clearshut():
+    sphd.clear()
+
+# This part captures the relevant shutdown signal to turn off Scroll pHAT HD on shutdown
+def handler(signum, frame):
+    clearshut()
+    exit(0)
+
+signal.signal(signal.SIGTERM, handler)
+signal.signal(signal.SIGINT, handler)
+
+# Use psutil to check CPU usage
 try:
     import psutil
 except ImportError:
     sys.exit("This script requires the psutil module\nInstall with: sudo pip install psutil")
 
+# Shortening the Scroll pHAT HD library
 import scrollphathd as sphd
 
-print("""
-Scroll pHAT HD: CPU
-
-Displays a graph with CPU values.
-
-Press Ctrl+C to exit!
-
-""")
-
+# Sets the display width correctly
 i = 0
-
 cpu_values = [0] * sphd.DISPLAY_WIDTH
 
-sphd.rotate(180)
+# Rotates the graph - uncomment if displays upside down depending on which USB port your power your Pi Zero from
+#sphd.rotate(180)
 
+# The main code - get CPU stats using psutil and then displays usage via the graph function on Scroll pHAT HD
 while True:
     try:
-        cpu_values.pop(0)
-        cpu_values.append(psutil.cpu_percent())
+	cpu_values.pop(0)
+	cpu_values.append(psutil.cpu_percent())
 
-        sphd.set_graph(cpu_values, low=0, high=25, brightness=0.25)
+	sphd.set_graph(cpu_values, low=0, high=25, brightness=0.25)
 
-        sphd.show()
-        time.sleep(0.2)
+	sphd.show()
+	time.sleep(0.2)
     except KeyboardInterrupt:
-        sphd.clear()
-        sys.exit(-1)
+	sphd.clear()
+	sys.exit(-1)
