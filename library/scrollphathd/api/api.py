224ac1a2fecb4741991ca0b9e0d5978e54537102
Godley <charlotte.godley@hotmail.co.uk>
2017-11-20 20:49:18 +0000

    feat: begin implementing flask blueprint

---
+++ library/scrollphathd/api/api.py
@@ -0,0 +1,50 @@
+from sys import exit
+import scrollphathd
+from scrollphathd.fonts import font3x5
+from queue import Queue
+from threading import Thread
+from .action import Action
+
+try:
+    from flask import Blueprint, render_template, abort
+except ImportError:
+    exit("flask must be installed in order to use the api. Install with pip install flask")
+
+scrollhat = Blueprint('scrollhat', __name__)
+api_queue = Queue()
+
+@scrollhat.route('/scroll/<text>/<speed>')
+def scroll(text, speed):
+    api_queue.put(Action("scroll", {"text": text,
+                                    "speed": speed}))
+
+
+@scrollhat.route('/show/<text>')
+def show(text):
+    api_queue.put(Action("write", {"text": text}))
+
+
+@scrollhat.route('/clear')
+def clear():
+    api_queue.put(Action("clear", {}))
+
+
+def run():
+    while True:
+        action = api_queue.get(block=True)
+        if action.action_type == "write":
+            scrollphathd.write_string(action.data["text"], font=font3x5)
+            scrollphathd.show()
+
+        if action.action_type == "clear":
+            scrollphathd.clear()
+
+        if action.action_type == "scroll":
+            scrollphathd.write_string(action.data["text"], font=font3x5)
+            scrollphathd.show()
+            # TODO scrolling behaviour?
+
+def start_background_thread():
+    api_thread = Thread(target=run())
+    api_thread.start()
+    api_thread.join()
