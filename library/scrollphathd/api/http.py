4cc2c1b895469e85a2ccf2b00d6103053173bc2a
Godley <charlotte.godley@hotmail.co.uk>
2017-11-22 19:41:40 +0000

    fix: python2 compat, various in response to code review + pycharm
    - changed to HTTPStatus in py3 version, pycharm doesn't seem to think original was right
    - add json response to OK responses. Also add similar "result" key to errors giving the exception type, ensures consistency of data output
    - support for json data or form data
    - catch keyerror on flip
    - move show down to cover all of the possible actions
    
    fix: jsonify all the dictionaries

--- library/scrollphathd/api/http.py
+++ library/scrollphathd/api/http.py
@@ -8,10 +8,14 @@ except ImportError:
 
 from .action import Action
 from .stoppablethread import StoppableThread
-from http import HTTPStatus
 
 try:
-    from flask import Blueprint, render_template, abort, request
+    import http.HTTPStatus as http_status
+except ImportError:
+    import httplib as http_status
+
+try:
+    from flask import Blueprint, render_template, abort, request, jsonify
 except ImportError:
     raise ImportError("flask must be installed in order to use the api. Install with pip install flask")
 
@@ -20,57 +24,83 @@ api_queue = Queue()
 
 @scrollphathd_blueprint.route('/scroll', methods=["POST"])
 def scroll():
+    response = {"result": "success"}
+    status_code = http_status.OK.value
+
     data = request.get_json()
+    if data is None:
+        data = request.form
     try:
         api_queue.put(Action("scroll", (data["x"], data["y"])))
     except KeyError:
-        return {"error": "keys x and y not posted."}, HTTPStatus.UNPROCESSABLE_ENTITY.value
-    else:
-        return HTTPStatus.OK.value
+        response = {"result": "KeyError", "error": "keys x and y not posted."}
+        status_code = http_status.UNPROCESSABLE_ENTITY.value
+
+    return jsonify(response), status_code
 
 
 @scrollphathd_blueprint.route('/show', methods=["POST"])
 def show(text):
+    response = {"result": "success"}
+    status_code = http_status.OK.value
+
     data = request.get_json()
+    if data is None:
+        data = request.form
     try:
         api_queue.put(Action("write", data["text"]))
     except KeyError:
-        return {"error": "key 'text' not set"}, HTTPStatus.UNPROCESSABLE_ENTITY.value
-    else:
-        return HTTPStatus.OK.value
+        response = {"result": "KeyError", "error": "key 'text' not set"}
+        status_code = http_status.UNPROCESSABLE_ENTITY.value
+
+    return jsonify(response), status_code
+
 
 @scrollphathd_blueprint.route('/clear', methods=["POST"])
 def clear():
+    response = {"result": "success"}
+    status_code = http_status.OK.value
+
     api_queue.put(Action("clear", {}))
-    return HTTPStatus.OK.value
+    return jsonify(response), status_code
+
 
 @scrollphathd_blueprint.route('/flip', methods=["POST"])
 def flip():
+    response = {"result": "success"}
+    status_code = http_status.OK.value
+
     data = request.get_json()
+    if data is None:
+        data = request.form
     try:
         api_queue.put(Action("flip", (bool(data["x"]), bool(data["y"]))))
     except TypeError:
-        return {"error": "Could not cast data correctly. Both `x` and `y` must be set to true or false."}, \
-               HTTPStatus.UNPROCESSABLE_ENTITY.value
-    else:
-        return HTTPStatus.OK.value
+        response = {"result": "TypeError", "error": "Could not cast data correctly. Both `x` and `y` must be set to true or false."}
+        status_code = http_status.UNPROCESSABLE_ENTITY.value
+    except KeyError:
+        response = {"result": "KeyError", "error": "Could not cast data correctly. Both `x` and `y` must be in the posted json data."}
+        status_code = http_status.UNPROCESSABLE_ENTITY.value
+
+    return jsonify(response), status_code
+
 
 def run():
     while True:
         action = api_queue.get(block=True)
         if action.action_type == "write":
             scrollphathd.write_string(action.data, font=font3x5)
-            scrollphathd.show()
 
         if action.action_type == "clear":
             scrollphathd.clear()
 
         if action.action_type == "scroll":
             scrollphathd.scroll(action.data[0], action.data[1])
-            scrollphathd.show()
 
         if action.action_type == "flip":
             scrollphathd.flip(x=action.data[0], y=action.data[1])
+
+        scrollphathd.show()
 
 def start_background_thread():
     api_thread = StoppableThread(target=run)
