f847213fc21b85bbf645c5f7b8f7173376c9abdc
nubtwo <ubtown@gmail.com>
2018-07-22 23:53:12 +0200

    first version of game of life

---
+++ examples/gameoflife.py
@@ -0,0 +1,84 @@
+import scrollphathd as phat
+import time, random
+
+ysize = 7
+xsize = 17
+bright = 0.3
+matrix = {}
+
+
+def generatemap():
+    for y in range(-1, ysize + 1):
+        for x in range(-1, xsize + 1):
+            matrix[y, x] = {'now': 0, 'soon': 0}
+
+    for x in range(10):
+        matrix[3, x + 3]['now'] = 1
+
+    # random light on
+    # for _ in range(100):
+    #     y = random.randint(0, ysize - 1)
+    #     x = random.randint(0, xsize - 1)
+    #     matrix[y, x]['now'] = 1
+
+
+def printmap(sleeptime):
+    active_counter = 0
+    phat.clear()
+
+    for y in range(0, ysize):
+        for x in range(0, xsize):
+            if matrix[y, x]['now']:
+                phat.set_pixel(x, y, bright)
+                active_counter += 1
+            else:
+                phat.set_pixel(x, y, 0)
+    print(active_counter)
+    phat.show()
+    time.sleep(sleeptime)
+    phat.clear()
+
+    # return active_counter
+
+
+def lifecycle():
+    for y in range(ysize):
+        for x in range(xsize):
+            neighbors = countneighbors(y, x, matrix[y, x]['now'])
+            if matrix[y, x]['now']:
+                matrix[y, x]['soon'] = 1 if 1 < neighbors < 4 else 0
+            else:
+                matrix[y, x]['soon'] = 1 if neighbors == 3 else 0
+            matrix[y, x]['now'] = matrix[y, x]['soon']
+
+
+def countneighbors(py, px, status):
+    neighbors = -1 if status else 0
+    for y in range(py - 1, py + 2):
+        for x in range(px - 1, px + 2):
+            if matrix[y, x]['now']:
+                neighbors += 1
+    return neighbors
+
+
+
+while True:
+
+    generatemap()
+    printmap(2)
+    stagnation = 0
+    stagnation_max = 10
+    active_count_old = 0
+
+    while True:
+        lifecycle()
+        printmap(2)
+        active_count = printmap(0.3)
+        if active_count_old == active_count:
+            stagnation += 1
+            print("ONE DOWN #" + str(stagnation))
+        active_count_old = active_count
+        print(active_count)
+
+        if stagnation == stagnation_max:
+            break
