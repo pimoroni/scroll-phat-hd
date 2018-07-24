70159a75784e7f079b7996ba3c330d10b3638a64
nubtwo <ubtown@gmail.com>
2018-07-24 22:15:31 +0200

    cleaned up code for matrix and comments

--- examples/gameoflife.py
+++ examples/gameoflife.py
@@ -1,87 +1,100 @@
 import scrollphathd as phat
 import time, random
 
-ysize = 7
-xsize = 17
-bright = 0.3
-matrix = {}
+YSIZE = 7
+XSIZE = 17
+BRIGHT = 0.3
+
+# Control update speed: 1 fastest - 5 slowest
+SPEED = 3
+
+
+print("""
+Scroll pHAT HD: GameOfLife
+
+Plays Conway's Game of Life. 
+Starts with random live cells and stops when stagnation is detected
+or the maximum amount of iterations is reached.
+
+Press Ctrl+C to exit!
+""")
 
 
 def generatemap():
-    for y in range(-1, ysize + 1):
-        for x in range(-1, xsize + 1):
-            matrix[y, x] = {'now': 0, 'soon': 0}
+    for y in range(-1, YSIZE + 1):
+        for x in range(-1, XSIZE + 1):
+            matrix[y, x] = 0
 
-    # random light on
-    # todo beautify
+    # crude way to start with live cells, but adds count diversity
     for _ in range(100):
-        y = random.randint(0, ysize - 1)
-        x = random.randint(0, xsize - 1)
-        matrix[y, x]['now'] = 1
-
-    # for x in range(10):
-    #     matrix[3, x + 3]['now'] = 1
+        y = random.randint(0, YSIZE - 1)
+        x = random.randint(0, XSIZE - 1)
+        matrix[y, x] = 1
 
 
 def printmap(sleeptime):
-    active_counter = 0
+    alive_counter = 0
     phat.clear()
 
-    for y in range(0, ysize):
-        for x in range(0, xsize):
-            if matrix[y, x]['now']:
-                phat.set_pixel(x, y, bright)
-                active_counter += 1
+    for y in range(0, YSIZE):
+        for x in range(0, XSIZE):
+            if matrix[y, x]:
+                phat.set_pixel(x, y, BRIGHT)
+                alive_counter += 1
             else:
                 phat.set_pixel(x, y, 0)
+
     phat.show()
-    time.sleep(sleeptime)
+    time.sleep(sleeptime / 10)
     phat.clear()
 
-    return active_counter
+    return alive_counter
 
 
 def lifecycle():
     # preparing next cycle
-    for y in range(ysize):
-        for x in range(xsize):
-            neighbors = countneighbors(y, x, matrix[y, x]['now'])
-            if matrix[y, x]['now']:
-                matrix[y, x]['soon'] = 1 if 1 < neighbors < 4 else 0
+    soonmatrix = {key: value for key, value in matrix.items()}
+    for y in range(YSIZE):
+        for x in range(XSIZE):
+            neighbors = countneighbors(y, x, matrix[y, x])
+            if matrix[y, x]:
+                soonmatrix[y, x] = 1 if 1 < neighbors < 4 else 0
             else:
-                matrix[y, x]['soon'] = 1 if neighbors == 3 else 0
+                soonmatrix[y, x] = 1 if neighbors == 3 else 0
 
     # activating next cycle
-    for y in range(ysize):
-        for x in range(xsize):
-            matrix[y, x]['now'] = matrix[y, x]['soon']
+    matrix.update(soonmatrix)
 
 
 def countneighbors(py, px, status):
+    # Counts live neighbors by checking within a 3x3 grid
     neighbors = -1 if status else 0
     for y in range(py - 1, py + 2):
         for x in range(px - 1, px + 2):
-            if matrix[y, x]['now']:
+            if matrix[y, x]:
                 neighbors += 1
     return neighbors
 
 
 while True:
+    matrix = {}
     max_iterations = 100
     stagnation_max = 10
-
-    active_count_old = 0
+    alive_count_old = 0
     stagnation_count = 0
+
     generatemap()
-    printmap(2)
+    printmap(20)
+
     while True:
         lifecycle()
-        active_count = printmap(0.1)
-        if active_count_old == active_count:
+        active_count = printmap(SPEED)
+
+        # checks if the amount of live cells changed
+        if alive_count_old == active_count:
             stagnation_count += 1
-        active_count_old = active_count
-        print("iter #{}, stagnation #{}".format(max_iterations, stagnation_count))
+        alive_count_old = active_count
         max_iterations -= 1
 
-        if stagnation_count == stagnation_max or max_iterations < 0:
+        if stagnation_count == stagnation_max or max_iterations < 1:
             break
